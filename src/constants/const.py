d7a01c7aef30a70ceaee0dedb18d8c45c98f73c8
Aakash <aak6.ash@gmail.com>
2024-04-14 14:02:09 -0500

    added 3DMOT notebook

--- src/constants/const.py
+++ src/constants/const.py
@@ -6,6 +6,9 @@ h = 6.62607015 * 10**(-34)
 h_bar = h / (2 * np.pi)
 kB = 1.380649 * 10**(-23)
 
+# Material constants
+rho_cu = 1.7 * 10**(-8)
+
 # 171Yb constants
 gamma_1s0_1p1 = 2 * np.pi * 29.1 * 10**6
 Is_1s0_1p1 = 59.97 * 10**(-3) * 10**(4)
