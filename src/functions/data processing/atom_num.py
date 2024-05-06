631fd0f5c9c2d3befe73be550e9c2cd7fae6f0cd
Aakashv6 <aak6.ash@gmail.com>
2024-05-06 11:52:00 -0500

    fixed fit_data() & dark_count() in atom_num.py

--- src/functions/data processing/atom_num.py
+++ src/functions/data processing/atom_num.py
@@ -1,4 +1,6 @@
 import sys, os
+sys.path.append('../YBII')
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,7 +15,6 @@ import matplotlib.pyplot as plt
 from src.functions.math import func as mathf
 from src.functions.optics import func as optf
 from src.constants import const
-
 
 # p is the measured power per beam before any VP, w1 and w2 are the beam waist radii
 # s0 = 2 * (I_0 * T + I_0 * T ** 3) / I_sat
@@ -44,7 +45,8 @@ def getAtomNumber(x0, y0, wx, wy, img, t_exp, f, p, d0):
     eff = (0.0254/2) ** 2 / 4 / d0 ** 2 # collection efficiency of the imaging setup, assuming using 1 inch lens
     gamma_tot = I_sum * ppi / eff / t_exp # total photon emission rate
     gamma_atom = const.gamma_1s0_1p1 * rho(f, p) # photon emission rate of a single atom
-    print(rho(f, p))
+    # print(rho(f, p))
+
     return gamma_tot / gamma_atom
 
 # get the number of imaged atoms
@@ -61,6 +63,8 @@ def getImagedAtomNumber(img, img_bg, t_exp, f, p, d0, fit_override=None, param_i
     y_rg = np.arange(0, img.shape[0])
 
     img_res = np.abs(np.array(img, dtype=float) - np.array(img_bg, dtype=float))
+    # img_res = np.array(img, dtype=float) - np.array(img_bg, dtype=float)
+    # img_res[img_res < 0] = 0
 
     x_data = np.max(img_res, axis=0)
     y_data = np.max(img_res, axis=1)
@@ -117,60 +121,107 @@ def getImagedAtomNumber(img, img_bg, t_exp, f, p, d0, fit_override=None, param_i
     wx = 2 * result_x.best_values['sigma']
     wy = 2 * result_y.best_values['sigma']
 
+    bgx = result_x.best_values['c_c']
+    bgy = result_y.best_values['c_c']
+
     if fit_override!=None:
         x0 = fit_override[0]
         y0 = fit_override[1]
         wx = fit_override[2]
         wy = fit_override[3]
 
-    return img_res, x0, y0, wx, wy, x_data, x_fit, y_data, y_fit, getAtomNumber(x0, y0, wx, wy, img_res, t_exp, f, p, d0)
+    return img_res, x0, y0, wx, wy, bgx, bgy, x_data, x_fit, y_data, y_fit, getAtomNumber(x0, y0, wx, wy, img_res, t_exp, f, p, d0)
 
 # plot the image data along with the Gaussian fit and the atom number
 # img is the MOT image data, img_bg is the background image data
 # t_exp is the exposure time of the camera
 # df is the detuning from the Yb171 1S0 -> 1P1 transition in angular frequency units
 # p is the measured power per beam before any VP
-def plotMOTNumber(img, img_bg, f_data, t_exp, df, p, d0, fit_override=None, param_init=None, constraints=None, plot_save=True):
+def plotMOTNumber(img, img_bg, f_data, t_exp, df, p, d0, fit_override=None, param_init=None, constraints=None, plot_save=True, save_dir="./results/2DMOT_atom_numbers/"):
     
-    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    img_res, x0, y0, wx, wy, bgx, bgy, x_data, x_fit, y_data, y_fit, atom_num = getImagedAtomNumber(img, img_bg, t_exp, df, p, d0, fit_override, param_init, constraints)
 
-    x_rg = np.arange(0, img.shape[1])
-    y_rg = np.arange(0, img.shape[0])
+    if True:
+        fig, axs = plt.subplots(1, 2, figsize=(10, 5))
 
-    img_res, x0, y0, wx, wy, x_data, x_fit, y_data, y_fit, atom_num = getImagedAtomNumber(img, img_bg, t_exp, df, p, d0)
+        x_rg = np.arange(0, img.shape[1])
+        y_rg = np.arange(0, img.shape[0])
 
-    axs[0].imshow(img_res, cmap='gray', vmin=0, vmax=255)
+        axs[0].imshow(img_res, cmap='gray', vmin=0, vmax=255)
 
-    axs[0].set_xlim([0, img.shape[1]])
-    axs[0].set_ylim([0, img.shape[0]])
-    axs[0].set_xlabel('Pixel')
-    axs[0].set_ylabel('Pixel')
-    axs[0].set_title(f_data)
+        axs[0].set_xlim([0, img.shape[1]])
+        axs[0].set_ylim([0, img.shape[0]])
+        axs[0].set_xlabel('Pixel')
+        axs[0].set_ylabel('Pixel')
+        axs[0].set_title(f_data)
 
-    axs[0].hlines(y0-wy, x0-wx, x0+wx, color='b', linestyles='dashed')
-    axs[0].hlines(y0+wy, x0-wx, x0+wx, color='b', linestyles='dashed')
-    axs[0].vlines(x0-wx, y0-wy, y0+wy, color='r', linestyles='dashed')
-    axs[0].vlines(x0+wx, y0-wy, y0+wy, color='r', linestyles='dashed')
+        axs[0].hlines(y0-wy, x0-wx, x0+wx, color='b', linestyles='dashed')
+        axs[0].hlines(y0+wy, x0-wx, x0+wx, color='b', linestyles='dashed')
+        axs[0].vlines(x0-wx, y0-wy, y0+wy, color='r', linestyles='dashed')
+        axs[0].vlines(x0+wx, y0-wy, y0+wy, color='r', linestyles='dashed')
 
-    axs[1].scatter(x_rg, x_data)
-    axs[1].plot(x_rg, x_fit, 'r', label='x Fit')
-    axs[1].scatter(y_rg, y_data)
-    axs[1].plot(y_rg, y_fit, 'b', label='y Fit')
-    axs[1].vlines(x0-wx, 0, 100, color='r', linestyles='dashed', label='x bounds')
-    axs[1].vlines(x0+wx, 0, 100, color='r', linestyles='dashed')
-    axs[1].vlines(y0-wy, 0, 100, color='b', linestyles='dashed', label='y bounds')
-    axs[1].vlines(y0+wy, 0, 100, color='b', linestyles='dashed')
-    
-    axs[1].set_xlabel('Pixel')
-    axs[1].set_ylabel('Intensity')
-    axs[1].legend()
+        axs[1].scatter(x_rg, x_data)
+        axs[1].plot(x_rg, x_fit, 'r', label='x Fit')
+        axs[1].scatter(y_rg, y_data)
+        axs[1].plot(y_rg, y_fit, 'b', label='y Fit')
+        axs[1].vlines(x0-wx, 0, 100, color='r', linestyles='dashed', label='x bounds')
+        axs[1].vlines(x0+wx, 0, 100, color='r', linestyles='dashed')
+        axs[1].vlines(y0-wy, 0, 100, color='b', linestyles='dashed', label='y bounds')
+        axs[1].vlines(y0+wy, 0, 100, color='b', linestyles='dashed')
+        
+        axs[1].set_xlabel('Pixel')
+        axs[1].set_ylabel('Intensity')
+        axs[1].legend()
 
-    fig.suptitle('2D MOT #Atom ~ ' + str(int(round(atom_num, -4))/(10**3)) + "K - X0:"+str(x0)+", Y0:"+str(y0)+", wx:"+str(wx)+", wy:"+str(wy) + " in px")
+        fig.suptitle('2D MOT #Atom ~ ' + str(int(round(atom_num, -4))/(10**3)) + "K - X0:"+str(round(x0, 2))+", Y0:"+str(str(round(y0, 2)))+", wx:"+str(str(round(wx, 2)))+", wy:"+str(str(round(wy, 2))) + " in px")
 
     if plot_save == True:
-        plt.savefig(f_data[:, -4] + '.png')
+        plt.savefig(save_dir + f_data[:-4] + '.png')
 
-    return atom_num
+    return img_res, x0, y0, wx, wy, bgx, bgy, x_data, x_fit, y_data, y_fit, atom_num
+
+
+def var_extract(dir, keywords=[["exp", 1e-3], ["power", 1e-3], ["fL", 1e12], ["delf", 1e6]], save_params=True):
+
+    # Get a list of file names in the directory
+    file_names = os.listdir(dir)
+
+    # Filter to include only files, not directories
+    file_names = [file for file in file_names if os.path.isfile(os.path.join(dir, file))]
+    # Further filter to find files ending with '_bg'
+    bg_files = [file for file in file_names if file.endswith("_bg.bmp")]
+
+    exp_params = []
+
+    for f in bg_files:
+        f_data = f[:-7] + ".bmp"
+
+        vars_file = []
+
+        split_vars = f_data.split("_")
+        vars_file.append(f_data)
+
+        for k in keywords:
+            try:
+                s = split_vars.index(k[0]) + 1
+                e = s + 2
+            except:
+                print("Parameter not in the file name")
+                continue
+
+            val = float(".".join(split_vars[s : e])) * k[1]
+            vars_file.append(val)
+
+        exp_params.append(vars_file)
+
+        exp_params = np.array(exp_params)
+
+        if save_params:
+            np.savetxt("C:/Users/aak6a/YbII/results/" + "exp_params.csv", exp_params, delimiter=",")
+
+    print(exp_params)
+
+# var_extract("C:/Users/aak6a/YbII/data/generated_2DMOT_images/")
 
 def fit_data(dir, t_exp, df, p, d0, fit_override=None, param_init=None, constraints=None, plot_save=True, save_data=False):
 
@@ -182,15 +233,37 @@ def fit_data(dir, t_exp, df, p, d0, fit_override=None, param_init=None, constrai
     # Further filter to find files ending with '_bg'
     bg_files = [file for file in file_names if file.endswith("_bg.bmp")]
 
-    # Now bg_files variable contains all file names that end with '_bg' in the specified directory
-    print(bg_files)
-
     for f in bg_files:
 
         f_data = f[:-7] + ".bmp"
         f_bg = f
 
-        img_data = imageio.imread(f_data)
-        img_bg = imageio.imread(f_bg)
+        img_data = imageio.imread(dir + f_data)
+        img_bg = imageio.imread(dir + f_bg)
 
-        atom_num = plotMOTNumber(img_data, img_bg, f_data, t_exp, df, p, d0, fit_override=None, param_init=None, constraints=None, plot_save=True)
+        atom_num = plotMOTNumber(img_data, img_bg, f_data, t_exp, df, p, d0, fit_override, param_init, constraints, plot_save)
+
+# fit_data("C:/Users/aak6a/YbII/data/generated_2DMOT_images/", 17150e-6, 2 * np.pi * 751.5270397e12, 80e-3, 350e-3)
+
+def dark_count():
+    # Using fitting constant
+    dir = "C:/Users/aak6a/YbII/data/generated_2DMOT_images/"
+
+    img_data = imageio.imread(dir + "2DMOT_0000.bmp")
+    img_bg = imageio.imread(dir + "2DMOT_0000_bg.bmp")
+
+    f_data = "2DMOT_0000.png"
+
+    t_exp = 17150e-6
+    df = 2 * np.pi * 751.5270397e12
+    p = 80e-3
+    d0 = 350e-3
+
+    img_res, x0, y0, wx, wy, bgx, bgy, x_data, x_fit, y_data, y_fit, atom_num = plotMOTNumber(img_data, img_bg, f_data, t_exp, df, p, d0, fit_override=None, param_init=None, constraints=None, plot_save=False)
+    
+    area = (2 * wx) * (2 * wy)
+    noise_bg = ((bgx + bgy)/2) * area
+
+    return noise_bg
+
+# dark_count()
